0800598114216fc6b7ca163ba8c242fe73d6ab06
tazmaniandevil64 <44484776+tazmaniandevil64@users.noreply.github.com>
2021-02-19 14:46:49 -0600

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,21 @@
+import os
+import urllib.request
+
+FilePath = './http_access_log.txt'
+FileExists = os.path.exists(FilePath)
+
+if FileExists == False:
+    print("It doesn't appear you have the data, downloading it now")
+    url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+    urllib.request.urlretrieve(url,"./http_access_log.txt")
+
+else:
+    print ("It looks like you already have the file")
+
+print ("Reviewing the data now")
+TextFile = open("http_access_log.txt")
+Log = TextFile.read()
+FirstYearCount = Log.count("1994")
+SecondYearCount = Log.count("1995")
+print("Number of accsesses from 1994:", FirstYearCount)
+print("Number of accsesses from 1994 and 1995:", FirstYearCount + SecondYearCount)
